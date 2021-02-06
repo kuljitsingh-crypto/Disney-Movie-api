676f2497d8b484d33bcc8102714788e099a22ae4
Kuljit Singh <78553633+kuljitsingh-crypto@users.noreply.github.com>
2021-02-06 21:15:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flaskserver import server
-
+from flaskserver import create_app()
+app=create_app()
 if __name__=="__main__":
-	server.run(host='0.0.0.0',port="5000",threaded=True)
+	app.run(host='0.0.0.0',port="5000",threaded=True)
